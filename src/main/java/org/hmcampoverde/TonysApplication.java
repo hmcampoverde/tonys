391eee2495db9cdef591b2ddf23f53364a87f7d6
Henry Manuel Campoverde Hidalgo <hmcampoverde@gmail.com>
2025-07-19 16:41:57 -0500

    Inicio del proyecto

---
+++ src/main/java/org/hmcampoverde/TonysApplication.java
@@ -0,0 +1,13 @@
+package org.hmcampoverde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TonysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hmcampoverde/TonysApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hmcampoverde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TonysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
